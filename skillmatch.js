b458b1b094c7b94eb31065ecaed27e281cf1e10a
Mariana Gomes <94244730+Mocca-lk@users.noreply.github.com>
2026-05-19 19:57:04 -0300

    feat: Classificação de Compatibilidade - Testada - OK

--- skillmatch.js
+++ skillmatch.js
@@ -54,9 +54,22 @@ vagas.forEach(vaga => {
     const totalRequisitosVaga = vaga.requisitos.length
     const porcentagem = (habilidadesCompativeis.length / totalRequisitosVaga) * 100
 
+    //Usando estrutura if/else para armazenar nível de compatibilidade
+    let statusCompatibilidade = ""
+
+        if (porcentagem >= 80) {
+            statusCompatibilidade = "Alta Compatibilidade 😎"
+        } else if (porcentagem >= 50 && porcentagem <= 79) {
+            statusCompatibilidade = "Média Compatibilidade 🫤"
+        } else {
+            statusCompatibilidade = "Baixa Compatibilidade 😪"
+        }
+
+
     console.log(`
         Empresa: ${vaga.empresa}
         Vaga: ${vaga.cargo}
+        Classificação: ${statusCompatibilidade}
         Compatibilidade de: ${porcentagem.toFixed(0)}% 
         Habilidades Encontradas: ${habilidadesCompativeis}
         Habilidades Faltantes: ${habilidadesFaltantes}
