7554c818b719bfebdf9206648f236a5d80eaf8d8
Mariana Gomes <94244730+Mocca-lk@users.noreply.github.com>
2026-05-19 18:10:51 -0300

    feat: estrutura inicial - perfilUsuário

---
+++ skillmatch.js
@@ -0,0 +1,8 @@
+//Criação do perfil/um objeto para representar o candidato:
+
+const candidato = {
+    nome: "Maria",
+    area: "Front-End",
+    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Trello"],
+    experienciaMeses: 3
+}
