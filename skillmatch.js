0ca9513c0e4f2317e9108f482806272d45821a2c
Mariana Gomes <94244730+Mocca-lk@users.noreply.github.com>
2026-05-19 18:29:02 -0300

    feat: Vagas Adicionadas - Testado - OK

--- skillmatch.js
+++ skillmatch.js
@@ -7,4 +7,37 @@ const candidato = {
     experienciaMeses: 3
 }
 
-console.log(candidato)
+// console.log(candidato) - Teste para validação da feature
+
+//Criação da lista/array de vagas
+const vagas = [
+    {
+        id: 1,
+        empresa: "TechStart",
+        cargo: "Desenvolvedor Front-End Júnior",
+        requisitos: ["JavaScript", "GitHub", "Lógica de Programação"],
+        salario: 2800,
+        modalidade: "Remoto"
+    },
+
+    {
+        id: 2,
+        empresa: "CodeLab",
+        cargo: "Estágio Front-End",
+        requisitos: ["JavaScript", "GitHub", "Kanban"],
+        salario: 1800,
+        modalidade: "Híbrido"
+    },
+
+    {
+        id: 3,
+        empresa: "WebSolutions",
+        cargo: "Programador JavaScript Júnior",
+        requisitos: ["JavaScript", "Arrays", "Objetos", "Funções"],
+        salario: 3000,
+        modalidade: "Presencial"
+    }
+]
+
+
+// console.log(vagas[0]) - Teste de validação feature Vagas
